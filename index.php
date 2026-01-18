3b30f0c6635d8d09e807cb9cf919aa0ac3c43e5d
jophyfeng23 <jophyfeng1104@gmail.com>
2026-01-18 22:18:51 +0800

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,34 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <title>Burger Malas - Makan? Malas...</title>
-    <link rel="icon" href="assets/img/BMalas_Favicon.jpg" type="image/jpg">
-
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-
-
-    <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
-
-
-    <!-- SEO -->
-    <meta name="description"
-        content="Burger Malas serves fresh, juicy burgers, big portion burgers, crispy fries, mushrooms, malas bowl, fillets and burger set in Penang. Franchise available.">
-    <meta name="keywords"
-        content="burger, malas bowl, fries, mushrooms, fillets, fast food, Penang, Mainland, Burger Malas">
-
-    <!-- Custom CSS -->
-    <link href="css/stylesheet.css" rel="stylesheet">
-
-
-
-</head>
+<?php include 'partials/header.php'; ?>
 
 <body>
 
@@ -276,6 +246,7 @@
 
 
 </html>
+
 
 
 
