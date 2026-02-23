3fdd0410b06792be830688080ad7a2bd26e02ff8
jophyfeng23 <jophyfeng1104@gmail.com>
2026-02-23 20:06:42 +0800

    Update location.php

--- location.php
+++ location.php
@@ -7,11 +7,11 @@
 
         <div class="text-center mb-5">
             <h1 class="fw-bold text-dark mb-3">Find Burger Malas</h1>
-            <p class="text-muted">Too Malas to cook? We relate. Find your nearest Burger Malas and let us handle the grill.</p>
+            <p class="subText">Too Malas to cook? We relate. Find your nearest Burger Malas and let us handle the grill.</p>
         </div>
 
-        <ul class="nav nav-pills nav-pills-custom justify-content-center mb-5" id="locationTabs" role="tablist">
-            <li class="nav-item mx-2" role="presentation">
+        <ul class="nav nav-pills nav-pills-custom justify-content-center mb-5 gap-3 " id="locationTabs" role="tablist">
+            <li class="nav-item " role="presentation">
                 <button class="nav-link active" id="mainland-tab" data-bs-toggle="pill" data-bs-target="#mainland" type="button" role="tab" aria-controls="mainland" aria-selected="true">
                     📍 Penang Mainland
                 </button>
@@ -28,8 +28,8 @@
             <div class="tab-pane fade show active" id="mainland" role="tabpanel" aria-labelledby="mainland-tab">
                 <div class="row g-4">
 
-                    <div class="col-lg-6">
-                        <div class="clean-card">
+                    <div class="col-lg-6 ">
+                        <div class="clean-card ">
                             <h4 class="fw-bold mb-4 text-dark">Simpang Ampat</h4>
                             <div class="info-row">
                                 <i class="bi bi-geo-alt-fill"></i>
@@ -45,7 +45,7 @@
                             </div>
 
                             <div class="action-row">
-                                <a href="#" class="btn btn-outline-dark btn-clean"><i class="bi bi-map"></i> Map</a>
+                                <a href="https://maps.app.goo.gl/wVxyeBCeF7W4uGMX6" class="btn btn-outline-dark btn-clean"><i class="bi bi-map"></i> Map</a>
                                 <a href="https://wa.me/60102490882" class="btn btn-success btn-clean"><i class="bi bi-whatsapp"></i> Order Now</a>
                                 <a href="https://www.foodpanda.my/restaurant/y540/burger-malas-taman-merak-jaya" class="btn btn-fp btn-clean">
                                     <img width="18" src="https://img.icons8.com/fluency/48/foodpanda.png" alt="fp" /> Foodpanda
@@ -71,7 +71,7 @@
                             </div>
 
                             <div class="action-row">
-                                <a href="#" class="btn btn-outline-dark btn-clean"><i class="bi bi-map"></i> Map</a>
+                                <a href="https://maps.app.goo.gl/Bq5DEfTWGCCPxACW9" class="btn btn-outline-dark btn-clean"><i class="bi bi-map"></i> Map</a>
                                 <a href="https://wa.me/601116456687" class="btn btn-success btn-clean"><i class="bi bi-whatsapp"></i> Order Now</a>
                                 <a href="https://www.foodpanda.my/restaurant/pdxl/burger-malas-raja-uda" class="btn btn-fp btn-clean">
                                     <img width="18" src="https://img.icons8.com/fluency/48/foodpanda.png" alt="fp" /> Foodpanda
@@ -97,7 +97,7 @@
                             </div>
 
                             <div class="action-row">
-                                <a href="#" class="btn btn-outline-dark btn-clean"><i class="bi bi-map"></i> Map</a>
+                                <a href="https://maps.app.goo.gl/s9nbYaJbjBonJVkY7" class="btn btn-outline-dark btn-clean"><i class="bi bi-map"></i> Map</a>
                                 <a href="https://wa.me/60197504323" class="btn btn-success btn-clean"><i class="bi bi-whatsapp"></i> Order Now</a>
                                 <a href="https://www.foodpanda.my/restaurant/e6mu/burger-malas-alma" class="btn btn-fp btn-clean">
                                     <img width="18" src="https://img.icons8.com/fluency/48/foodpanda.png" alt="fp" /> Foodpanda
@@ -123,7 +123,7 @@
                             </div>
 
                             <div class="action-row">
-                                <a href="#" class="btn btn-outline-dark btn-clean"><i class="bi bi-map"></i> Map</a>
+                                <a href="https://maps.app.goo.gl/wBuSCyZUx1b99sX97" class="btn btn-outline-dark btn-clean"><i class="bi bi-map"></i> Map</a>
                                 <a href="https://wa.me/60166273589" class="btn btn-success btn-clean"><i class="bi bi-whatsapp"></i> Order Now</a>
                                 <a href="https://www.foodpanda.my/restaurant/hw4f/burger-malas-taman-puteri-gunung" class="btn btn-fp btn-clean">
                                     <img width="18" src="https://img.icons8.com/fluency/48/foodpanda.png" alt="fp" /> Foodpanda
@@ -149,7 +149,7 @@
                             </div>
 
                             <div class="action-row">
-                                <a href="#" class="btn btn-outline-dark btn-clean"><i class="bi bi-map"></i> Map</a>
+                                <a href="https://maps.app.goo.gl/fjgXPvoqcFFk7pnQ9" class="btn btn-outline-dark btn-clean"><i class="bi bi-map"></i> Map</a>
                                 <a href="https://wa.me/601127238661" class="btn btn-success btn-clean"><i class="bi bi-whatsapp"></i> Order Now</a>
                                 <a href="https://www.foodpanda.my/restaurant/itql/burger-malas-taman-bukit-panchor" class="btn btn-fp btn-clean">
                                     <img width="18" src="https://img.icons8.com/fluency/48/foodpanda.png" alt="fp" /> Foodpanda
@@ -172,7 +172,7 @@
                                 <div class="text-start">
                                     <div class="info-row">
                                         <i class="bi bi-geo-alt-fill"></i>
-                                        <span>68, Jalan Free School,<br>11600 George Town, P.Pinang</span>
+                                        <span>68, Jln Free School,<br>11600 George Town, P.Pinang</span>
                                     </div>
                                     <div class="info-row">
                                         <i class="bi bi-clock-fill"></i>
@@ -186,7 +186,7 @@
                             </div>
 
                             <div class="action-row justify-content-center mt-4">
-                                <a href="#" class="btn btn-outline-dark btn-clean"><i class="bi bi-map"></i> Map</a>
+                                <a href="https://maps.app.goo.gl/wFuqUG5UcDVYyxVW7" class="btn btn-outline-dark btn-clean"><i class="bi bi-map"></i> Map</a>
                                 <a href="https://wa.me/60125238727" class="btn btn-success btn-clean"><i class="bi bi-whatsapp"></i> Order Now</a>
                                 <a href="https://www.foodpanda.my/restaurant/t0ae/burger-malas-jelutong" class="btn btn-fp btn-clean">
                                     <img width="18" src="https://img.icons8.com/fluency/48/foodpanda.png" alt="fp" /> Foodpanda
